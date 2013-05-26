e9d51034aaed811f98613d7e828172b67124da4a
Edwin Steele <edwin@wordspeak.org>
2013-05-26 16:55:17 +1000

    Fix package names and typo in linkchecker setup

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -4,7 +4,7 @@
 # Should be run before pip install -r requirements.txt
 
 # Must be root
-if $(whoami) == "root";
+if [ "$(whoami)" = "root" ];
 then
   echo "This script must be not run as root, it gets root permissions via sudo. Exiting".
   exit 1;
@@ -17,11 +17,11 @@ sudo yum install gcc python-devel
 sudo yum install enchant
 
 # To allow compilation of lxml
-sudo yum install libxmlt libxml-devel libxslt-devel
+sudo yum install libxslt libxml2-devel libxslt-devel
 
 # Manually install Linkchecker. It doesn't install via pip because it's distributed as an xz archive
 tempdir=$(mktemp -d)
 wget http://github.com/downloads/wummel/linkchecker/LinkChecker-8.4.tar.xz -O $tempdir/LinkChecker-8.4.tar.xz
 unxz $tempdir/LinkChecker-8.4.tar.xz
-pip install $tempdir/Linkchecker-8.4.tar
+pip install $tempdir/LinkChecker-8.4.tar
 rm -r $tempdir
