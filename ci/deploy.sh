8575fc2b117453a76fc297ed1e87cf1c3e6999e2
Edwin Steele <edwin-lists@wordspeak.org>
2017-12-30 17:12:18 +1100

    Run for all branches. Do special stuff on master

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "deploying $TRAVIS_BRANCH"
+deploy_to_staging( ) {
+  echo "Deploying to staging"; 
+}
+
+deploy_to_prod( ) {
+  echo "Deploying to prod";
+}
+
+echo "Running with TRAVIS_BRANCH = $TRAVIS_BRANCH"
+deploy_to_staging
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  deploy_to_prod
+fi
