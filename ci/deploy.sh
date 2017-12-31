894738e4ee05c4c644e205e7c8f4f463ee1e3664
Edwin Steele <edwin-lists@wordspeak.org>
2017-12-31 11:12:35 +1100

    Reduce code duplication

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,55 +1,41 @@
 #!/bin/bash
 
-STAGING_DEPLOY_KEY="ci/staging-sync-id_rsa"
-PROD_DEPLOY_KEY="ci/prod-sync-id_rsa"
+STAGING_DEPLOY_KEY_ENC="ci/staging-sync-id_rsa.enc"
+PROD_DEPLOY_KEY_ENC="ci/prod-sync-id_rsa.enc"
+DEPLOY_KEY="ci/deploy-id_rsa"
+
 SSH_STANDARD_ARGS="-l esteele -o StrictHostKeyChecking=no -o BatchMode=yes"
 
-deploy_to_staging( ) {
-  echo "Deploying to staging";
-  rsync -av \
-    -e "ssh $SSH_STANDARD_ARGS -i $STAGING_DEPLOY_KEY" \
-    --delete \
-    --filter="protect language_explorer" \
-    --filter="exclude *.md" \
-    --filter="exclude *.md.gz" \
-    output/ \
-    origin.wordspeak.org:/home/esteele/Sites/staging.wordspeak.org/
-}
-
-deploy_to_prod( ) {
-  echo "Deploying to prod";
-  rsync -av \
-    -e "ssh $SSH_STANDARD_ARGS -i $PROD_DEPLOY_KEY" \
-    --delete \
-    --filter="protect language_explorer" \
-    --filter="exclude *.md" \
-    --filter="exclude *.md.gz" \
-    output/ \
-    origin.wordspeak.org:/home/esteele/Sites/www.wordspeak.org/
-}
+touch $DEPLOY_KEY
+chmod 600 $DEPLOY_KEY
 
 if [ "$1" = "staging" ]; then
-  touch $STAGING_DEPLOY_KEY
-  chmod 600 $STAGING_DEPLOY_KEY
-  openssl aes-256-cbc \
-    -K $encrypted_68de9d9be834_key \
-    -iv $encrypted_68de9d9be834_iv \
-    -in $STAGING_DEPLOY_KEY.enc \
-    -out $STAGING_DEPLOY_KEY \
-    -d
-  deploy_to_staging
-  rm $STAGING_DEPLOY_KEY
+  echo "Deploying to staging";
+  encrypted_key=$STAGING_DEPLOY_KEY_ENC
+  target_site="staging.wordspeak.org"
+elif [ "$1" = "prod" ]; then
+  echo "Deploying to prod";
+  encrypted_key=$PROD_DEPLOY_KEY_ENC
+  target_site="www.wordspeak.org"
+else
+  echo "Unknown deployment target. Exiting";
+  exit 1;
 fi
 
-if [ "$1" = "prod" ]; then
-  touch $PROD_DEPLOY_KEY
-  chmod 600 $PROD_DEPLOY_KEY
-  openssl aes-256-cbc \
-    -K $encrypted_68de9d9be834_key \
-    -iv $encrypted_68de9d9be834_iv \
-    -in $PROD_DEPLOY_KEY.enc \
-    -out $PROD_DEPLOY_KEY \
-    -d
-  deploy_to_prod
-  rm $PROD_DEPLOY_KEY
-fi
+openssl aes-256-cbc \
+  -K $encrypted_68de9d9be834_key \
+  -iv $encrypted_68de9d9be834_iv \
+  -in $encrypted_key \
+  -out $DEPLOY_KEY \
+  -d
+
+rsync -av \
+  -e "ssh $SSH_STANDARD_ARGS -i $DEPLOY_KEY" \
+  --delete \
+  --filter="protect language_explorer" \
+  --filter="exclude *.md" \
+  --filter="exclude *.md.gz" \
+  output/ \
+  origin.wordspeak.org:/home/esteele/Sites/$target_site/
+
+exit $?
