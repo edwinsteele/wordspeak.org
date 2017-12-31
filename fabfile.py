a46a0c192fd925a310bb481347c158579995adca
Edwin Steele <edwin-lists@wordspeak.org>
2017-12-31 15:33:09 +1100

    Lots of unnecessary stuff

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,25 @@
-from __future__ import print_function
-from future import standard_library
-standard_library.install_aliases()
-from builtins import map
+#!/usr/bin/env python3
+"""Wordspeak build and deploy helper"""
+
 import glob
 import os
 import re
 from subprocess import DEVNULL, Popen, PIPE
-import socket
 import sys
 import time
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 from fabric.api import abort, local
-from fabric.colors import green, red, yellow
-from fabric.contrib.console import confirm
-from fabric.context_managers import cd, hide, quiet
-from fabric.operations import prompt
+from fabric.colors import green, yellow
+from fabric.context_managers import cd
 import enchant
 import enchant.checker
 import enchant.tokenize
 import requests
-import conf
 from chump import Application
-import webassets  # for bundle creation
-import rcssmin  # for css minifaction
+import conf
+
 
 TILDE = os.path.expanduser("~")
 STAGING_FQDN = "staging.wordspeak.org"
@@ -69,41 +66,6 @@ W3C_RSS_VALIDATION_TARGETS = [
 LANGUAGE_EXPLORER_DIRNAME = "language_explorer"
 
 
-def _does_this_machine_answer_for_this_hostname(dns_name):
-    """Looks at DNS and local interfaces to see if this host answers for the
-     DNS name in question
-
-    Caveats:
-    - Won't work reliably if the DNS entry resolves to more than one address
-    - Won't work if there are no PTR entries
-    - Assumes the interface configured with the IP associated with the host's
-      hostname is actually the interface that accepts public traffic
-      associated with DNS name in question
-    """
-    try:
-        my_main_ip = socket.gethostbyname(socket.getfqdn())
-    except socket.gaierror:
-        # Can't resolve hostname to a public IP, so we're probably going to
-        #  be referring to ourselves by localhost, so let's allocate an
-        #  IP address accordingly.
-        my_main_ip = "127.0.0.1"
-
-    # do a round-trip to so that we match when the host is behind a load
-    #  balancer and doesn't have a public IP address (assumes split-horizon
-    #  DNS is configured to resolve names to internal addresses) e.g. AWS
-    # If the dns_name resolves to 127.0.0.1, then whatever machine we're
-    #  on definitely answers for the hostname
-    try:
-        ptr_lookup_result = socket.gethostbyaddr(
-            socket.gethostbyname(dns_name))[0]
-    except socket.herror:
-        # No PTR records available and nothing in local /etc/hosts to help.
-        # Let's be conservative and say that we don't answer for it.
-        return False
-
-    return socket.gethostbyname(ptr_lookup_result) in (my_main_ip, '127.0.0.1')
-
-
 def _quietly_run_nikola_cmd(nikola, cmd):
     result = local("%s %s" % (nikola, cmd), capture=True)
     if "TaskFailed" in result.stdout or \
@@ -115,8 +77,8 @@ def _quietly_run_nikola_cmd(nikola, cmd):
         abort("Nikola command '%s' failed." % (cmd,))
     else:
         if result.stderr:
-            print("%s actions performed\n" %
-                  (len(result.stderr.splitlines()) - 1), end=' ')
+            print("%s actions performed" %
+                  (len(result.stderr.splitlines()) - 1,))
         else:
             print("No output from command.")
 
@@ -125,7 +87,7 @@ def build():
     """Build the site using nikola"""
     with cd(SITE_BASE):
         _quietly_run_nikola_cmd("nikola", "build")
-        #_quietly_run_nikola_cmd("nikola", "mincss")
+        # _quietly_run_nikola_cmd("nikola", "mincss")
         # Need to recopy the leaflet.css file as mincss optimises it away
         #  because it can't find any leaflet classes in use (they're inserted
         #  at runtime by the js library
@@ -136,97 +98,6 @@ def build():
         _quietly_run_nikola_cmd("nikola", "build")
 
     post_build_cleanup()
-
-
-def requirements_dump():
-    """pip freeze the package requirements"""
-    with cd(SITE_BASE):
-        # pyinotify and MacFSEvents only build on their particular platform
-        #  so exclude them. They'll get pulled in when a pip install doit
-        #  is done so there's no loss.
-        # bsddb3 is only necessary on MacOS and is a pain to build, so we
-        #  don't use it. This requires a quick change in doit.
-        #
-        # Uncomment the following line in site-packages/doit/dependency.py
-        #
-        # import gdbm as ddbm
-        #
-        # It might be necessary to nuke the doit db file after this change:
-        local("pip freeze | "
-              "egrep -v '(pyinotify|MacFSEvents|bsddb3|wheel)' | "
-              "sort -f "
-              "> requirements.txt")
-
-
-def maybe_add_untracked_files(is_interactive_deploy):
-    """Look for untracked files in the repo and give option to add"""
-    with cd(SITE_BASE):
-        result = local("git status --porcelain", capture=True)
-
-    for line in result.stdout.splitlines():
-        if line[0:2] == "??":
-            if is_interactive_deploy:
-                if not confirm("Add untracked file '%s'?" % (line[3:],)):
-                    continue
-            else:
-                print("Adding untracked file '%s' during non-interactive"
-                      " deploy" % (line[3:],))
-
-            with cd(SITE_BASE):
-                local("git add '%s'" % (line[3:],))
-
-
-def repo_status(is_interactive_deploy):
-    """Check whether there are any uncommitted/untracked files in the repo"""
-    with cd(SITE_BASE):
-        result = local("git status --porcelain", capture=True)
-    if result.stdout:
-        print(result.stdout)
-        if is_interactive_deploy:
-            response = prompt("Repo has uncommitted/untracked files. "
-                              "'abort' to abort or type a commit message",
-                              default="abort").strip()
-            if response == "abort":
-                abort("Aborting at user request.")
-        else:
-            response = "Auto-commit during non-interactive deploy"
-
-        with cd(SITE_BASE):
-            local("git commit -a -m'%s'" % (response,))
-
-
-def _sync_site(destination_path):
-    with cd(SITE_BASE):
-        local('rsync '
-              '--delete '
-              '--filter="protect %s" '
-              '--filter="exclude *.md" '
-              '--filter="exclude *.md.gz" '
-              '-a %s/ %s' %
-              (LANGUAGE_EXPLORER_DIRNAME, OUTPUT_BASE, destination_path))
-
-
-def staging_sync():
-    """Sync the site to the staging server"""
-    if _does_this_machine_answer_for_this_hostname(STAGING_FQDN):
-        destination = STAGING_RSYNC_DESTINATION_LOCAL
-    else:
-        destination = STAGING_RSYNC_DESTINATION_REMOTE
-
-    _sync_site(destination)
-    local("rsync -a %s/staging_robots.txt %s/robots.txt" %
-          (SITE_BASE, destination))
-
-
-def prod_sync():
-    """Sync the site to the prod server"""
-    with cd(SITE_BASE):
-        local("rsync -a %s/prod_robots.txt %s/robots.txt" %
-              (SITE_BASE, OUTPUT_BASE))
-    if _does_this_machine_answer_for_this_hostname(PROD_FQDN):
-        _sync_site(PROD_RSYNC_DESTINATION_LOCAL)
-    else:
-        _sync_site(PROD_RSYNC_DESTINATION_REMOTE)
 
 
 def linkchecker(output_fd=sys.stdout):
@@ -243,7 +114,7 @@ def linkchecker(output_fd=sys.stdout):
         "--find-sources"
     ]
 
-    with Popen(args, stderr=PIPE, stdout=DEVNULL, bufsize=0) as proc:
+    with Popen(args, stderr=PIPE, stdout=DEVNULL) as proc:
         while proc.poll() is None:
             # Print something to indicate progress to the build
             print(".", end="", flush=True)
@@ -278,31 +149,6 @@ def linkchecker(output_fd=sys.stdout):
         output_fd.write(green("No broken links found.\n"))
         print_warning_lines(warning_lines, output_fd)
         sys.exit(0)
-
-
-def repo_push():
-    """Push the wordspeak repo to github"""
-    local("git push")
-
-
-def repo_pull():
-    """Get changes from git in this repo.
-     Deliberately uses https to avoid needing keys"""
-    result = local(
-        "git pull https://github.com/edwinsteele/wordspeak.org.git master",
-        capture=True)
-    print(result.stderr)
-    print(result.stdout)
-    # Something like:
-    #
-    # Updating 815b459..d9a508d
-    # Fast-forward
-    # fabfile.py       | 2 +-
-    # requirements.txt | 1 -
-    # 2 files changed, 1 insertion(+), 2 deletions(-)
-
-    # return something that evaluates to true if we updated a key file
-    return [k for k in KEY_FILES if k in result.stdout]
 
 
 def clean():
@@ -413,8 +259,9 @@ def spellchecker(is_interactive_deploy=True):
         with open(file_to_check, 'r', encoding="utf-8") as f:
             lines = f.readlines()
 
-        e = _get_spellcheck_exceptions(lines)
-        list(map(pwl_dictionary.add_to_session, e))
+        for exc in _get_spellcheck_exceptions(lines):
+            pwl_dictionary.add_to_session(exc)
+
         for line in _non_directive_lines(lines):
             en_spellchecker.set_text(strip_markdown_directives(line))
             for err in en_spellchecker:
@@ -429,19 +276,8 @@ def spellchecker(is_interactive_deploy=True):
                          ", ".join(en_spellchecker.suggest(err.word))
                          )
                     print(spelling_error)
-                    if is_interactive_deploy:
-                        action = prompt("Add '%s' to dictionary [add] or "
-                                        "replace [type replacement]?"
-                                        % (err.word,), default="add").strip()
-                        if action == "add":
-                            _add_to_spellcheck_exceptions(file_to_check,
-                                                          err.word)
-                            pwl_dictionary.add(err.word)
-                        else:
-                            _replace_in_file(file_to_check, err.word, action)
-                    else:
-                        _send_pushover_summary(
-                            spelling_error, "Spelling error: %s" % (err.word,))
+                    _send_pushover_summary(
+                        spelling_error, "Spelling error: %s" % (err.word,))
 
     return spelling_errors_found
 
@@ -493,8 +329,10 @@ def w3c_checks(output_fd=sys.stdout):
             output_fd.write("RSS validates (%s)\n" % (url,))
         else:
             output_fd.write("RSS validation failures for %s\n" % (url,))
-            output_fd.write("Full details: %s\n" % (W3C_RSS_VALIDATION_URL %
-                                                    (urllib.parse.quote_plus(url))))
+            output_fd.write(
+                "Full details: %s\n" %
+                (W3C_RSS_VALIDATION_URL % (urllib.parse.quote_plus(url)))
+            )
             all_checks_pass = False
 
     if all_checks_pass:
@@ -530,41 +368,3 @@ def _send_pushover_summary(message, title):
     app = Application(get_env_variable("WORDSPEAK_PUSHOVER_API_TOKEN"))
     user = app.get_user(get_env_variable("WORDSPEAK_PUSHOVER_USER"))
     return user.send_message(message=message, title=title)
-
-
-def deploy(is_interactive_deploy=True):
-    """Runs all the pre-deployment checks, pushing to staging and then prod"""
-    spellcheck_needed = True
-    key_files_changed = repo_pull()
-    if key_files_changed:
-        abort(red("Aborting as the following key files changed: %s" %
-                  (",".join(key_files_changed),)))
-    maybe_add_untracked_files(is_interactive_deploy)
-    _initialise()
-    while spellcheck_needed:
-        # Re-run spellchecker if changes were found, but only if we're doing
-        #  an interactive deployment
-        spellcheck_needed = spellchecker(is_interactive_deploy)
-        if spellcheck_needed and not is_interactive_deploy:
-            abort("Spellcheck errors during non-interactive deploy."
-                  " Unable to proceed")
-    build()
-    requirements_dump()
-    repo_status(is_interactive_deploy)
-    staging_sync()
-    if not is_interactive_deploy or confirm("Push to live site?"):
-        prod_sync()
-        repo_push()
-    else:
-        print(red("Not pushing to live site."))
-
-
-def non_interactive_deploy():
-    """Helper method to trigger non-interactive deployment"""
-    if sys.stdin.isatty():
-        if not confirm("Simulate a non-interactive deploy?", default=False):
-            abort("Aborting")
-    else:
-        print("Running non-interactive deploy")
-
-    deploy(is_interactive_deploy=False)
